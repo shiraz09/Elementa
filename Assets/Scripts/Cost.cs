60e91d0072e71bc7ce2626fa15bd347de4da6f91
shirazNagaoker <118122538+shiraz09@users.noreply.github.com>
2025-09-10 22:29:10 +0300

    store

--- Assets/Scripts/Cost.cs
+++ Assets/Scripts/Cost.cs
@@ -5,4 +5,10 @@ public struct Cost
     public int sun;
     public int earth;
     public int grass;
+    public static Cost Multiply(Cost c, int k) => new Cost {
+        water = c.water * k,
+        sun   = c.sun   * k,
+        earth = c.earth * k,
+        grass = c.grass * k
+    };
 }
